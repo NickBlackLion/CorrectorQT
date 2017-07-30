56dc8dc3456956cab998f5f852cd820ebcf76000
Nick <nicknight88@live.com>
2017-07-30 10:12:54 +0300

    Added: .gitignore Changed: fileMenu.py

--- fileMenu.py
+++ fileMenu.py
@@ -23,6 +23,11 @@ class FileMenu(QMenu):
         self.__exitAction()
 
         self.fileName = None
+        self.star = False
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.__changeTitleIfModified)
+        self.timer.start(50)
 
     def __newAction(self):
         self.newAction = QAction('Новый', self)
@@ -57,8 +62,9 @@ class FileMenu(QMenu):
         self.addAction(self.exitAction)
 
     def __open(self):
-        self.__saveIfChanged()
+        self.__saveIfChanged(self.__openFunction)
 
+    def __openFunction(self):
         self.fileName = QFileDialog.getOpenFileName(self.parent, '', '', "Document Microsoft Word (*.docx)")
 
         if self.fileName is not None and self.fileName[0]:
@@ -66,22 +72,22 @@ class FileMenu(QMenu):
             for val in doc.paragraphs:
                 self.cursor.insertText(val.text)
 
+        self.doc.setModified(False)
         self.mainWindow.setWindowTitle(self.mainWindow.windowTitle() + '-' + self.fileName[0])
 
     def __saveAs(self):
         self.fileName = QFileDialog.getSaveFileName(self.parent, '', '', "Document Microsoft Word (*.docx)")
 
         if self.fileName is not None and self.fileName[0]:
-            doc = Document()
-            for val in self.doc.toPlainText().split('\n'):
-                doc.add_paragraph(val)
-
-            doc.save(self.fileName[0])
+            self.__addToDocx()
 
     def __new(self):
-        self.__saveIfChanged()
+        self.__saveIfChanged(isNewFunction=True)
 
     def __exit(self):
+        self.__saveIfChanged(self.__exitFunction)
+
+    def __exitFunction(self):
         self.app.quit()
 
     def __message(self, windTitle, text, infoText):
@@ -94,23 +100,48 @@ class FileMenu(QMenu):
 
         return hex(info.exec_())
 
-    def __saveIfChanged(self):
+    def __saveIfChanged(self, function=None, isNewFunction=False):
         if self.doc.isModified():
             retval = self.__message('Изменение текста', 'Файл изменен', 'Вы хотите сохранить изменения')
             if retval == hex(0x10000):
                 self.doc.clear()
                 self.doc.setModified(False)
+                self.mainWindow.setWindowTitle('Корректор')
+                self.fileName = None
+                self.star = False
+                if not isNewFunction:
+                    function()
+            elif retval == hex(0x4000):
+                self.__save()
+                if not isNewFunction:
+                    function()
             elif retval == hex(0x400000):
                 return
-            elif retval == hex(0x400):
-                pass
+        else:
+            self.doc.clear()
+            self.doc.setModified(False)
+            self.mainWindow.setWindowTitle('Корректор')
+            self.fileName = None
+            if not isNewFunction:
+                function()
 
     def __save(self):
         if self.fileName is not None and self.fileName[0]:
-            doc = Document()
-            for val in self.doc.toPlainText().split('\n'):
-                doc.add_paragraph(val)
-
-            doc.save(self.fileName[0])
+            self.__addToDocx()
         else:
             self.__saveAs()
+
+    def __addToDocx(self):
+        doc = Document()
+        for val in self.doc.toPlainText().split('\n'):
+            doc.add_paragraph(val)
+
+        doc.save(self.fileName[0])
+        self.doc.setModified(False)
+        self.star = False
+        self.mainWindow.setWindowTitle('Корректор' + '-' + self.fileName[0])
+
+    def __changeTitleIfModified(self):
+        if self.doc.isModified() and not self.star:
+            self.mainWindow.setWindowTitle(self.mainWindow.windowTitle() + '*')
+            self.star = True
